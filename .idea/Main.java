c4070975f00998f3c1e64248e30f6c396976eebd
Hellen Machado Borba <102324118+HellenBorba@users.noreply.github.com>
2024-11-17 23:57:59 -0300

    Código DAO, Entidades e Controller
    DAO:
    Criado os files de DAO.
    Controller:
    Criado o file Categoria.

---
+++ .idea/Main.java
@@ -0,0 +1,9 @@
+package br.com.Projeto_ProcessosProjetosTI.Controle_Estoque.idea.Main;
+
+
+public class Main {
+    public static void Main(String[] args)
+    {
+        System.out.println("ola");
+    }
+}
--- .idea/Modelo/Entidades/Perfil.java
+++ .idea/Modelo/Entidades/Perfil.java
@@ -1,4 +1,3 @@
-
 public enum PERFIL {
     ADMIN,
     PADRAO;
