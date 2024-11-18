686adcb0bdb0829341088b6242defd0fda3ca143
Hellen Machado Borba <102324118+HellenBorba@users.noreply.github.com>
2024-11-18 00:16:05 -0300

    Teste
    Falha

--- .idea/ViewTesteHellen/Formulario/Dashboard.java
+++ .idea/ViewTesteHellen/Formulario/Dashboard.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package com.culysoft.gestaovenda.view.formulario;
+package br.com.Projeto_ProcessosProjetosTI.Controle_Estoque.idea.ViewTesteHellen.Dashboard;
 
-//import com.culysoft.gestaovenda.modelo.controller.CategoriaController;
+import br.com.Projeto_ProcessosProjetosTI.Controle_Estoque.idea.Modelo.Controller.CategoriaController;
 //import com.culysoft.gestaovenda.modelo.controller.ClienteController;
 //import com.culysoft.gestaovenda.modelo.controller.DashboardController;
 //import com.culysoft.gestaovenda.modelo.controller.ProdutoController;
